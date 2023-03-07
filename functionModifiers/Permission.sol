5c4c08996001a20ed3df7ee051a25fafc6f32246
lucianoV <luvp95@hotmail.com>
2023-03-06 22:46:33 -0300

    pequeños_cambios

--- functionModifiers/Permission.sol
+++ functionModifiers/Permission.sol
@@ -4,17 +4,21 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Permission {
     address private owner;
-    string public projectName = "Platzi";
+    string public projectName = "PruebaCrow";
 
     constructor() {
         owner = msg.sender;
+        
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can change the project name");
+        require(msg.sender == owner, 
+        "Only owner can change the project name");
         //la función es insertada en donde aparece este símbolo
-        _;
-    }
+        _; //identifica de donde se insertara el codigo de la funcion, despues del requiere
+
+    }   
+
 
     function changeProjectName(string memory _projectName) public onlyOwner {
         projectName = _projectName;
